dcaf5400cffda695223e0d7b024a7670bbf4f71a
Jhonny <jbarriossandrea@gmail.com>
2018-08-06 19:30:01 -0400

    Cambie los selectores javascript por selectores JQuery Formatee los strings para sustituir las variables sin tener que salir de las cadenas de texto Implemente en la interfaz grafica los datos de ciudad y temperatura Voy a poner un manejador de eventos para el elemento "Degrees"

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,13 +1,12 @@
-window.onload = getMylocation;
-function getMylocation(){
-	var climas;
-if (navigator.geolocation){
-  	navigator.geolocation.getCurrentPosition(function(position) {
-  	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-    getWeatherData(latitude,longitude);	
-  }, errorHandler);
-}
+$(document).ready(function(){
+		if (navigator.geolocation){
+	  	navigator.geolocation.getCurrentPosition(function(position) {
+	  	latitude = position.coords.latitude;
+		longitude = position.coords.longitude;
+	    getWeatherData(latitude,longitude);	
+	  }, errorHandler);
+	}
+
 	function errorHandler(error){
 		var errorTypes = {
 			0 : "Unknown Error",
@@ -22,17 +21,57 @@ if (navigator.geolocation){
 			errorMessage = errorMessage + " " + error.message;
 		}
 
-		var message = document.getElementById("data");
-		message.innerHTML = errorMessage;
+		alert(errorMessage);
 	}
-}
+
+	$("#degrees").click(function(){
+		if ($(this).value.indexOf("C") !== -1) {
+			
+		}
+	})
+});
 
 function getWeatherData(lat, lon){
-	var apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=8c483ac0ba0e00a23f343af80fbc493a";
+	var apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=8c483ac0ba0e00a23f343af80fbc493a`;
 	$.getJSON(apiURL, function (response){
-		console.log(response);
-		var dataDiv = document.getElementById('data');
-		dataDiv.innerHTML = response.name;
-		dataDiv.innerHTML += response.weather[0].id;
+		var cityName = response.name;
+		var celcius = Math.round(response.main.temp - 273.15);
+		var farenheit = Math.round(9*(response.main.temp-273.15)/5+32);
+		var responseData = `<h2>${cityName}</h2> <h2 id = "degrees" >${celcius}°C</h2>`;
+		$("#data").append(responseData);
+		$("#data h2").addClass("text-center text-white");
+		viewWeather(response.weather[0].main);
 	});
+}
+
+function viewWeather(weather){
+	$("#data").append("<h1><i></i></h1>");
+	var icon;
+	switch(weather)
+	{
+		case "Clear":
+			icon = "fas fa-sun";
+		break;
+
+		case "Rain":
+		case "Clouds":
+		case "Drizzle":
+			icon = "fas fa-cloud";
+		break;
+
+		case "Thunderstorm":
+			icon = "fas fa-bolt";
+		break;
+
+		case "Snow":
+			icon = "fas fa-snowflake";
+		break;
+
+		default:
+			icon = "text-danger fas fa-frown";
+		break;
+	}
+
+	$("#data h1 i").addClass(icon);
+	
 }
