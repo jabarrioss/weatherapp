48c6954820f2eb23dd98c505312a32522ecede19
Jhonny <jbarriossandrea@gmail.com>
2018-08-13 16:55:02 -0400

    I created a initialize function to give the program to an initial state I created a switch function to wait for it... Switch the degrees in the view

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,7 +1,19 @@
-/*
-Params = openweather API Object
-*/
-function WeatherData(params){
+$(document).ready(function(){
+	
+	function initialize(){
+		if (navigator.geolocation){
+  			navigator.geolocation.getCurrentPosition(function(position){
+		  	latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+			getWeatherData(latitude,longitude);
+	  		}, errorHandler);
+		}
+
+	}
+
+	var weatherObject;
+
+	function WeatherData(params){
 		this.cityName = params.name;
 		this.kelvin = params.main.temp;
 		this.weather = params.weather[0].main;
@@ -16,28 +28,17 @@ function WeatherData(params){
 
 	}
 
-//Global weather object for use in the program
-
-var weatherObject;
-
-$(document).ready(function(){
-	if (navigator.geolocation){
-  		navigator.geolocation.getCurrentPosition(function(position) {
-	  	latitude = position.coords.latitude;
-		longitude = position.coords.longitude;
-	    getWeatherData(latitude,longitude);
-	    var responseData = `<h2>${weatherObject.cityName}</h2> <h2 id = "degrees" >${weatherObject.getCelcius()}</h2>`;
-		$("#data").append(responseData);
-		$("#data h2").addClass("text-center text-white");
-		viewWeather(weatherObject.weather);
-	  }, errorHandler);
+	function getWeatherData(lat, lon){
+		var apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=8c483ac0ba0e00a23f343af80fbc493a`;
+		$.getJSON(apiURL, function (response){
+			weatherObject = new WeatherData(response);
+			var responseData = `<h2>${weatherObject.cityName}</h2> <h2 id = "degrees" >${weatherObject.getCelcius()}</h2>`;
+			$("#data").append(responseData);
+			$("#data h2").addClass("text-center text-white");
+			viewWeather(weatherObject.weather);
+		});
 	}
-
-	
-
-});
-
-function errorHandler(error){
+	function errorHandler(error){
 	var errorTypes = {
 		0 : "Unknown Error",
 		1 : "Permission denied by user",
@@ -49,46 +50,51 @@ function errorHandler(error){
 
 	if(error.code == 0 || error.code == 2) {
 		errorMessage = errorMessage + " " + error.message;
-	}
-
+		}
+ 
 	alert(errorMessage);
 }
+	//Function for getting render the icon based on the weather
+	function viewWeather(weatherConditions){
+		$("#data").append("<h1><i></i></h1>");
+		var icon;
+		switch(weatherConditions)
+		{
+			case "Clear":
+				icon = "fas fa-sun";
+			break;
 
-function getWeatherData(lat, lon){
-	var apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=8c483ac0ba0e00a23f343af80fbc493a`;
-	$.getJSON(apiURL, function (response){
-		weatherObject = new WeatherData(response);
-	});
-}
-//Function for getting render the icon based on the weather
-function viewWeather(weatherConditions){
-	$("#data").append("<h1><i></i></h1>");
-	var icon;
-	switch(weatherConditions)
-	{
-		case "Clear":
-			icon = "fas fa-sun";
-		break;
+			case "Rain":
+			case "Clouds":
+			case "Drizzle":
+				icon = "fas fa-cloud";
+			break;
 
-		case "Rain":
-		case "Clouds":
-		case "Drizzle":
-			icon = "fas fa-cloud";
-		break;
+			case "Thunderstorm":
+				icon = "fas fa-bolt";
+			break;
 
-		case "Thunderstorm":
-			icon = "fas fa-bolt";
-		break;
+			case "Snow":
+				icon = "fas fa-snowflake";
+			break;
 
-		case "Snow":
-			icon = "fas fa-snowflake";
-		break;
+			default:
+				icon = "text-danger fas fa-frown";
+			break;
+		}
 
-		default:
-			icon = "text-danger fas fa-frown";
-		break;
+		$("#data h1 i").addClass(icon);
+		
 	}
 
-	$("#data h1 i").addClass(icon);
-	
-}
+	$("#switch").click(function(){
+		if($("#degrees").text().indexOf("C") == -1 ){
+			$("#degrees").empty();
+			$("#degrees").append(weatherObject.getCelcius());
+		}else{
+			$("#degrees").empty();
+			$("#degrees").append(weatherObject.getFarenheit());
+		}
+	});
+	initialize();
+});
